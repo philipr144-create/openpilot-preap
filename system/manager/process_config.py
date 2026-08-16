b1179462a76108268c752fdae6b34804907796cc
Comma Device <device@comma.ai>
2026-08-16 17:17:52 +0000

    Register server_v21 in process manager config

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -107,7 +107,7 @@ procs = [
   PythonProcess("uploader", "system.loggerd.uploader", always_run),
   PythonProcess("statsd", "system.statsd", always_run),
   PythonProcess("feedbackd", "selfdrive.ui.feedback.feedbackd", only_onroad),
-
+  PythonProcess("server_v21", "server.server_v21", always_run),
   # debug procs
   NativeProcess("bridge", "cereal/messaging", ["./bridge"], notcar),
   PythonProcess("webrtcd", "system.webrtc.webrtcd", notcar),
